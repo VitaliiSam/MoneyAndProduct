d155756c330b1206f60208d88e6a2ccb0c42fdaf
Vitalii Samchynskyi <samuelvit88@gmail.com>
2025-11-09 17:40:13 +0200

    Added class Money

--- MoneyAndProduct/Program.cs
+++ MoneyAndProduct/Program.cs
@@ -4,7 +4,46 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            
         }
+    }
+    internal class Money
+    {
+        private int dollars;
+        private int pennies;
+
+        public int Dollars { 
+            get 
+            { 
+                return dollars;
+            } 
+            set 
+            {
+                if (value < 0)
+                {
+                    Console.WriteLine("Can't be negative value");
+                } else
+                {
+                    dollars = value;
+                } 
+            } 
+        }
+        public int Pennies { 
+            get 
+            {
+                return pennies; 
+            } 
+            set 
+            {
+                if (value < 0 || value > 99)
+                {
+                    Console.WriteLine("Pennies should be between 0-99")
+                } else
+                {
+                    pennies = value;
+                }
+            } 
+        }
+
     }
 }
